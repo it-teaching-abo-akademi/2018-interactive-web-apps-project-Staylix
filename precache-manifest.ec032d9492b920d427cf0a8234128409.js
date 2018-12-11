1c3d79c52ab5a19d6a0d49cd3e7f24db889bcf28
Staylix <32682876+Staylix@users.noreply.github.com>
2018-12-11 10:46:06 +0200

    Updates

---
+++ precache-manifest.ec032d9492b920d427cf0a8234128409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spms2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d4623f4d28c8c0abc5b7",
+    "url": "/spms2/static/js/runtime~main.d4623f4d.js"
+  },
+  {
+    "revision": "5c1a6d5572493f47a942",
+    "url": "/spms2/static/js/main.5c1a6d55.chunk.js"
+  },
+  {
+    "revision": "150005d2ee73d8051e1a",
+    "url": "/spms2/static/js/1.150005d2.chunk.js"
+  },
+  {
+    "revision": "eb419871bdae83677ebaa05c2eee7043",
+    "url": "/spms2/index.html"
+  }
+];
